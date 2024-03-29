3ea5b557ce7532b3d294b766e911aebe6cc23034
Aryan Malla <aryanmalla19@gmail.com>
2024-03-29 22:06:18 +0545

    Carousal

---
+++ src/components/Carousal.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
+
+function Carousel() {
+  const slides = [
+    'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+    'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+    'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+    'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
+    'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => setCurrentIndex((prevIndex) => (prevIndex === 0 ? slides.length - 1 : prevIndex - 1));
+  const nextSlide = () => setCurrentIndex((prevIndex) => (prevIndex === slides.length - 1 ? 0 : prevIndex + 1));
+  const goToSlide = (slideIndex) => setCurrentIndex(slideIndex);
+
+  return (
+    <div className='max-w-[1400px] h-[780px] w-full m-auto relative group'>
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex]})` }}
+        className='w-full h-4/6 md:h-full bg-center bg-cover duration-500'
+      ></div>
+      {/* Left Arrow */}
+      <div className='block md:hidden group-hover:block absolute top-[60%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer' onClick={prevSlide}>
+        <BsChevronCompactLeft size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div className='md:hidden block group-hover:block absolute top-[60%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer' onClick={nextSlide}>
+        <BsChevronCompactRight size={30} />
+      </div>
+      <div className='flex absolute md:top-[90%] top-[60%] w-full justify-center py-2'>
+        {slides.map((_, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className={`text-2xl cursor-pointer ${slideIndex === currentIndex ? 'text-blue-500' : 'text-gray-500'}`}
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import SocialMedia from '../components/SocialMedia'
+import Carousel from '../components/Carousal'
 
 function Home() {
   return (
     <div className='bg-second min-h-screen'>
         <Navbar/>
         <SocialMedia/>  
+        <Carousel />
     </div>
   ) 
 }
